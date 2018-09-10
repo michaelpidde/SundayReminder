ee6d831de7f63001efce59d7b7742bbc1cd10a95
Michael Pidde <michael.pidde@gmail.com>
2018-09-09 22:17:46 -0500

    Import

---
+++ SundayReminder/ModEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace SundayReminder
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            TimeEvents.AfterDayStarted += this.TimeEvents_AfterDayStarted;
+        }
+
+        private void TimeEvents_AfterDayStarted(object sender, EventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+
+            var date = SDate.Now();
+            if(date.DayOfWeek.ToString().Equals("Sunday"))
+                Game1.addHUDMessage(new HUDMessage("It's Sunday morning. Maybe you should relax and watch a cooking show.", 2));
+        }
+    }
+}
